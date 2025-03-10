4352c081d788979f566745ea34fa385dee29f18e
Enesuygurs <Enesuygurs>
2025-03-10 21:51:13 +0300

    Fixed time issues.

--- Ramazan 2025/Form1.Designer.cs
+++ Ramazan 2025/Form1.Designer.cs
@@ -27,86 +27,164 @@
         ///  the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            label1 = new Label();
-            label2 = new Label();
-            label3 = new Label();
-            label4 = new Label();
-            label5 = new Label();
+            components = new System.ComponentModel.Container();
+            lblTime1 = new Label();
+            lblTime2 = new Label();
+            lblTime3 = new Label();
+            lblTime4 = new Label();
+            lblTime5 = new Label();
+            Title = new Label();
+            btnClose = new Label();
+            btnSettings = new Label();
+            lblKalanZaman = new Label();
+            timerKalanSure = new System.Windows.Forms.Timer(components);
             SuspendLayout();
             // 
-            // label1
+            // lblTime1
             // 
-            label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
-            label1.Location = new Point(30, 33);
-            label1.Name = "label1";
-            label1.Size = new Size(51, 21);
-            label1.TabIndex = 0;
-            label1.Text = "label1";
+            lblTime1.AutoSize = true;
+            lblTime1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            lblTime1.ForeColor = Color.WhiteSmoke;
+            lblTime1.Location = new Point(29, 130);
+            lblTime1.Name = "lblTime1";
+            lblTime1.Size = new Size(51, 21);
+            lblTime1.TabIndex = 0;
+            lblTime1.Text = "label1";
             // 
-            // label2
+            // lblTime2
             // 
-            label2.AutoSize = true;
-            label2.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
-            label2.Location = new Point(30, 67);
-            label2.Name = "label2";
-            label2.Size = new Size(54, 21);
-            label2.TabIndex = 1;
-            label2.Text = "label2";
+            lblTime2.AutoSize = true;
+            lblTime2.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            lblTime2.ForeColor = Color.WhiteSmoke;
+            lblTime2.Location = new Point(29, 160);
+            lblTime2.Name = "lblTime2";
+            lblTime2.Size = new Size(54, 21);
+            lblTime2.TabIndex = 1;
+            lblTime2.Text = "label2";
             // 
-            // label3
+            // lblTime3
             // 
-            label3.AutoSize = true;
-            label3.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
-            label3.Location = new Point(30, 101);
-            label3.Name = "label3";
-            label3.Size = new Size(54, 21);
-            label3.TabIndex = 2;
-            label3.Text = "label3";
+            lblTime3.AutoSize = true;
+            lblTime3.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            lblTime3.ForeColor = Color.WhiteSmoke;
+            lblTime3.Location = new Point(29, 190);
+            lblTime3.Name = "lblTime3";
+            lblTime3.Size = new Size(54, 21);
+            lblTime3.TabIndex = 2;
+            lblTime3.Text = "label3";
             // 
-            // label4
+            // lblTime4
             // 
-            label4.AutoSize = true;
-            label4.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
-            label4.Location = new Point(30, 135);
-            label4.Name = "label4";
-            label4.Size = new Size(54, 21);
-            label4.TabIndex = 3;
-            label4.Text = "label4";
+            lblTime4.AutoSize = true;
+            lblTime4.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            lblTime4.ForeColor = Color.WhiteSmoke;
+            lblTime4.Location = new Point(29, 220);
+            lblTime4.Name = "lblTime4";
+            lblTime4.Size = new Size(54, 21);
+            lblTime4.TabIndex = 3;
+            lblTime4.Text = "label4";
             // 
-            // label5
+            // lblTime5
             // 
-            label5.AutoSize = true;
-            label5.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
-            label5.Location = new Point(30, 171);
-            label5.Name = "label5";
-            label5.Size = new Size(54, 21);
-            label5.TabIndex = 4;
-            label5.Text = "label5";
+            lblTime5.AutoSize = true;
+            lblTime5.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            lblTime5.ForeColor = Color.WhiteSmoke;
+            lblTime5.Location = new Point(29, 250);
+            lblTime5.Name = "lblTime5";
+            lblTime5.Size = new Size(54, 21);
+            lblTime5.TabIndex = 4;
+            lblTime5.Text = "label5";
+            // 
+            // Title
+            // 
+            Title.AutoSize = true;
+            Title.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            Title.ForeColor = Color.WhiteSmoke;
+            Title.Location = new Point(34, 18);
+            Title.Name = "Title";
+            Title.Size = new Size(152, 42);
+            Title.TabIndex = 5;
+            Title.Text = "2025\r\nRamazan İmsakiyesi";
+            Title.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // btnClose
+            // 
+            btnClose.AutoSize = true;
+            btnClose.Font = new Font("Webdings", 11.25F, FontStyle.Bold);
+            btnClose.ForeColor = Color.WhiteSmoke;
+            btnClose.Location = new Point(193, 3);
+            btnClose.Name = "btnClose";
+            btnClose.Size = new Size(25, 20);
+            btnClose.TabIndex = 6;
+            btnClose.Text = "r";
+            btnClose.Click += btnClose_Click;
+            // 
+            // btnSettings
+            // 
+            btnSettings.AutoSize = true;
+            btnSettings.Font = new Font("Segoe UI Symbol", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnSettings.ForeColor = Color.WhiteSmoke;
+            btnSettings.Location = new Point(174, 3);
+            btnSettings.Name = "btnSettings";
+            btnSettings.Size = new Size(23, 20);
+            btnSettings.TabIndex = 7;
+            btnSettings.Text = "⚙";
+            btnSettings.Click += btnSettings_Click;
+            // 
+            // lblKalanZaman
+            // 
+            lblKalanZaman.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            lblKalanZaman.ForeColor = Color.WhiteSmoke;
+            lblKalanZaman.Location = new Point(50, 69);
+            lblKalanZaman.Name = "lblKalanZaman";
+            lblKalanZaman.Size = new Size(120, 42);
+            lblKalanZaman.TabIndex = 8;
+            lblKalanZaman.Text = "Kalan Süre:\r\n08:02:25";
+            lblKalanZaman.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // timerKalanSure
+            // 
+            timerKalanSure.Enabled = true;
+            timerKalanSure.Interval = 1000;
+            timerKalanSure.Tick += timerKalanSure_Tick;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(278, 319);
-            Controls.Add(label5);
-            Controls.Add(label4);
-            Controls.Add(label3);
-            Controls.Add(label2);
-            Controls.Add(label1);
+            BackColor = Color.Black;
+            ClientSize = new Size(220, 300);
+            Controls.Add(lblKalanZaman);
+            Controls.Add(btnSettings);
+            Controls.Add(btnClose);
+            Controls.Add(Title);
+            Controls.Add(lblTime5);
+            Controls.Add(lblTime4);
+            Controls.Add(lblTime3);
+            Controls.Add(lblTime2);
+            Controls.Add(lblTime1);
+            FormBorderStyle = FormBorderStyle.None;
             Name = "Form1";
+            Opacity = 0.75D;
             Text = "Ramazan 2025";
             Load += Form1_Load_1;
+            MouseDown += Form1_MouseDown;
+            MouseMove += Form1_MouseMove;
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
-        private Label label1;
-        private Label label2;
-        private Label label3;
-        private Label label4;
-        private Label label5;
+        private Label lblTime1;
+        private Label lblTime2;
+        private Label lblTime3;
+        private Label lblTime4;
+        private Label lblTime5;
+        private Label Title;
+        private Label btnClose;
+        private Label btnSettings;
+        private Label lblKalanZaman;
+        private System.Windows.Forms.Timer timerKalanSure;
     }
 }
--- Ramazan 2025/Form1.cs
+++ Ramazan 2025/Form1.cs
@@ -5,57 +5,105 @@ using Newtonsoft.Json;
 using System.Windows.Forms;
 using System.Reflection.Emit;
 
-
 namespace Ramazan_2025
 {
     public partial class Form1 : Form {
+
         public Form1() {
             InitializeComponent();
         }
-        public class NamazVakitleri {
-            public string Fajr { get; set; }
-            public string Dhuhr { get; set; }
-            public string Asr { get; set; }
-            public string Maghrib { get; set; }
-            public string Isha { get; set; }
-        }
-        private async void GetNamazVakitleri(string city) {
-            // Aladhan API'si için URL'yi oluþturuyoruz
-            string url = $"http://api.aladhan.com/v1/timingsByCity?city={city}&country=Turkey&method=13";
 
+        private string Fajr, Dhuhr, Asr, Maghrib, Isha;
+        private string TomorrowFajr; // Yarýnýn sahur vakti için deðiþken
+        private DateTime sonKontrolEdilenGun = DateTime.Now.Date; // Baþlangýçta bugünü saklýyoruz
+        private DateTime sahurVakti, iftarVakti, yarinSahurVakti;
+        private System.Windows.Forms.Label aktifLabel = null;
+
+        private async Task GetNamazVakitleri(string city) {
             using (HttpClient client = new HttpClient()) {
-                var response = await client.GetStringAsync(url);
-                dynamic data = JsonConvert.DeserializeObject(response);
+                string todayDate = DateTime.Now.ToString("dd-MM-yyyy");
+                string tomorrowDate = DateTime.Now.AddDays(1).ToString("dd-MM-yyyy");
 
-                // API'den alýnan namaz vakitlerini iþliyoruz
-                NamazVakitleri namazVakitleri = new NamazVakitleri {
-                    Fajr = data.data.timings.Fajr,
-                    Dhuhr = data.data.timings.Dhuhr,
-                    Asr = data.data.timings.Asr,
-                    Maghrib = data.data.timings.Maghrib,
-                    Isha = data.data.timings.Isha
-                };
+                string todayUrl = $"http://api.aladhan.com/v1/timingsByCity/{todayDate}?city={city}&country=Turkey&method=13";
+                string tomorrowUrl = $"http://api.aladhan.com/v1/timingsByCity/{tomorrowDate}?city={city}&country=Turkey&method=13";
 
-                // Vakitleri arayüze yazdýrýyoruz
-                label1.Text = "Ýmsak: " + namazVakitleri.Fajr;
-                label2.Text = "Öðle: " + namazVakitleri.Dhuhr;
-                label3.Text = "Ýkindi: " + namazVakitleri.Asr;
-                label4.Text = "Akþam: " + namazVakitleri.Maghrib;
-                label5.Text = "Yatsý: " + namazVakitleri.Isha;
+                var todayResponse = await client.GetStringAsync(todayUrl);
+                var tomorrowResponse = await client.GetStringAsync(tomorrowUrl);
+
+                dynamic todayData = JsonConvert.DeserializeObject(todayResponse);
+                dynamic tomorrowData = JsonConvert.DeserializeObject(tomorrowResponse);
+
+                // Bugünün namaz vakitlerini al
+                Fajr = todayData.data.timings.Fajr;
+                Dhuhr = todayData.data.timings.Dhuhr;
+                Asr = todayData.data.timings.Asr;
+                Maghrib = todayData.data.timings.Maghrib;
+                Isha = todayData.data.timings.Isha;
+
+                // Yarýnýn sahur vakti
+                TomorrowFajr = tomorrowData.data.timings.Fajr;
+                // Vakitleri arayüze yazdýr
+                lblTime1.Text = $"Ýmsak: {Fajr}";
+                lblTime2.Text = $"Öðle: {Dhuhr}";
+                lblTime3.Text = $"Ýkindi: {Asr}";
+                lblTime4.Text = $"Akþam: {Maghrib}";
+                lblTime5.Text = $"Yatsý: {Isha}";
             }
         }
 
-        private void btnGetVakitler_Click(object sender, EventArgs e) {
+        private async void timerKalanSure_Tick(object sender, EventArgs e) {
+            DateTime simdikiZaman = DateTime.Now;
+            DateTime today = simdikiZaman.Date;
 
+            // Eðer gün deðiþtiyse, namaz vakitlerini güncelle
+            if (sonKontrolEdilenGun != today) {
+                await GetNamazVakitleri("Sakarya"); // Yeni namaz vakitlerini çek
+                sonKontrolEdilenGun = today; // Son kontrol edilen günü güncelle
+            }
+
+            // Bugüne eklenmiþ saatleri oluþtur
+            sahurVakti = today.Add(sahurVakti.TimeOfDay);
+            iftarVakti = today.Add(iftarVakti.TimeOfDay);
+            yarinSahurVakti = today.AddDays(1).Add(yarinSahurVakti.TimeOfDay);
+
+            System.Windows.Forms.Label yeniAktifLabel = null;
+
+            if (simdikiZaman < sahurVakti) {
+                TimeSpan kalanSure = sahurVakti - simdikiZaman;
+                lblKalanZaman.Text = $"Kalan Süre:\n{kalanSure.Hours:D2}:{kalanSure.Minutes:D2}:{kalanSure.Seconds:D2}";
+                yeniAktifLabel = lblTime1;
+            } else if (simdikiZaman > sahurVakti && simdikiZaman < iftarVakti) {
+                TimeSpan kalanSure = iftarVakti - simdikiZaman;
+                lblKalanZaman.Text = $"Kalan Süre:\n{kalanSure.Hours:D2}:{kalanSure.Minutes:D2}:{kalanSure.Seconds:D2}";
+                yeniAktifLabel = lblTime4;
+            } else {
+                TimeSpan kalanSure = yarinSahurVakti - simdikiZaman;
+                lblKalanZaman.Text = $"Kalan Süre:\n{kalanSure.Hours:D2}:{kalanSure.Minutes:D2}:{kalanSure.Seconds:D2}";
+                yeniAktifLabel = lblTime1;
+            }
+
+            if (aktifLabel != yeniAktifLabel) {
+                if (aktifLabel != null) aktifLabel.ForeColor = Color.WhiteSmoke;
+                if (yeniAktifLabel != null) {
+                    yeniAktifLabel.ForeColor = Color.Red;
+                    aktifLabel = yeniAktifLabel;
+                }
+            }
         }
 
-        private void Form1_Load(object sender, EventArgs e) {
+        private async void Form1_Load_1(object sender, EventArgs e) {
+            await GetNamazVakitleri("Sakarya");
         }
 
-        private void Form1_Load_1(object sender, EventArgs e) {
-            GetNamazVakitleri("Sakarya");
-
+        private void btnSettings_Click(object sender, EventArgs e) {
+           
         }
+        private void btnClose_Click(object sender, EventArgs e) => Application.Exit();
+
+        #region Movable Form
+        private Point mouseLocation;
+        private void Form1_MouseMove(object sender, MouseEventArgs e) { if (e.Button == MouseButtons.Left) this.Location = new Point(Control.MousePosition.X + mouseLocation.X, Control.MousePosition.Y + mouseLocation.Y); }
+        private void Form1_MouseDown(object sender, MouseEventArgs e) => mouseLocation = new Point(-e.X, -e.Y);
+        #endregion
     }
-
 }
